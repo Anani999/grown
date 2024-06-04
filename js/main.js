18e20326a969e3912e8ecd041c4235f4479a5d46
anani <ananipc9@gmail.com>
2024-06-04 12:00:46 +0530

    created site

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function togleDiv(){
+    var div = document.getElementById('menu-links');
+    if(div.style.display == 'none'){
+        div.style.display = 'block';
+    }else{
+        div.style.display = 'none';
+    }
+}
